afd9ff5bdeb20cc50f13061695cd414f8c268387
FTC5 <dan.m1512@gmail.com>
2020-05-31 17:21:30 +0300

    Museum.Web : Add Controller

---
+++ Museum.Web/Controllers/ExpositionController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Museum.BLL.Interfaces;
+using Museum.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Museum.Web.Controllers
+{
+    public class ExpositionController : ApiController
+    {
+        IMapper mapper;
+        IExpositionService expositionService;
+
+        public ExpositionController(IMapper mapper, IExpositionService expositionService)
+        {
+            this.mapper = mapper;
+            this.expositionService = expositionService;
+        }
+        [HttpGet]
+        public IHttpActionResult GetExposition(int id)
+        {
+            var exposition = mapper.Map<ExpositionModel>(expositionService.GetExpositionInfo(id));
+            return Ok(exposition);
+        }
+        [HttpPut]
+        public IHttpActionResult AddExposition([FromBody]ExpositionModel exposition)
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult DeleteExposition([FromBody]ExpositionModel exposition)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Museum.Web/Controllers/GrafikController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Museum.BLL.Interfaces;
+using Museum.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Museum.Web.Controllers
+{
+    public class GrafikController : ApiController
+    {
+        IMapper mapper;
+        IGrafikService grafikService;
+
+        public GrafikController(IMapper mapper, IGrafikService grafikService)
+        {
+            this.mapper = mapper;
+            this.grafikService = grafikService;
+        }
+        [HttpGet]
+        public IHttpActionResult GetGrafiks()
+        {
+            
+            var grafiks = mapper.Map<IEnumerable<GrafikModel>>(grafikService.GetGrafiks());
+            return Ok(grafiks);
+            
+        }
+        [HttpGet]
+        public IHttpActionResult FindExpositionByGrafiks(string name)
+        {
+            var grafiks = mapper.Map<IEnumerable<GrafikModel>>(grafikService.FindByName(name));
+            return Ok(grafiks);
+        }
+    }
+}
